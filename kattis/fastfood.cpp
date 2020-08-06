f8c1e16e0b56e508266d8a743240d230f8f7a510
GermainZH <59399144+GermainZH@users.noreply.github.com>
2020-08-06 15:24:25 -0400

    Add files via upload

--- kattis/fastfood.cpp
+++ kattis/fastfood.cpp
@@ -11,22 +11,44 @@ int main(){
     for(int i=0; i<t;++i){
         int n,m;
         cin>>n>>m;
-        vector<int> prizes[n];
-        int prizeV[n];
-        int stickers[m];
-        for(int y=0; y<n;++y){
+        vector<vector<int>> p;
+        int s[m];
+        for(int j=0; j<n;++j){
+            p.push_back(vector<int>());
             int k;
             cin>>k;
-            for(int j=0; j<k;++j){
-                int s;
-                cin>>s;
-                prizes[y].push_back(s);
+            for(int y=0; y<=k;++y){
+                int tmp;
+                cin>>tmp;
+                p[j].push_back(tmp);
             }
-            cin>>prizeV[y];
         }
-        for(int y=0; y<m;++y){
-            cin>>stickers[y];
+        for(int j=0; j<m;++j){
+            cin>>s[j];
         }
+        int total=0;
+        while(true){
+            bool redeemed=false;
+            for(int j=0; j<n;++j){
+                int y;
+                for(y=0; y<p[j].size()-1;++y){
+                    if(s[p[j][y]-1]<1){
+                        break;
+                    }
+                }
+                if(y==p[j].size()-1){
+                    redeemed=true;
+                    total+=p[j][p[j].size()-1];
+                    for(y=0; y<p[j].size()-1;++y){
+                        s[p[j][y]-1]--;
+                    }
+                }
+            }
+            if(!redeemed){
+                break;
+            }
+        }
+        cout<<total<<'\n';
     }
     return 0;
 }
